201748e238a913351e41c0983c47410223caa63f
nabroo101 <elnabawy101@gmail.com>
2023-07-04 16:20:38 -0400

    fixed an error in logic3

--- Starter_Code/static/js/logic3.js
+++ Starter_Code/static/js/logic3.js
@@ -9,7 +9,7 @@ let street = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 let topo = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
   attribution:
-    'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+     'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
 // Create a baseMaps object.
