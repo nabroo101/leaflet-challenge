4629088ee1de9a0bbac61d8b0c2d149842848652
nabroo101 <elnabawy101@gmail.com>
2023-07-04 12:28:08 -0400

    created layers and deployed map

---
+++ Starter_Code/static/js/logic1.js
@@ -0,0 +1,29 @@
+// testting server
+console.log("Hello World");
+
+// Create the base layers.
+let street = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+});
+
+let topo = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+  attribution:
+    'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+});
+
+// Create a baseMaps object.
+let baseMaps = {
+  "Street Map": street,
+  "Topographic Map": topo,
+};
+
+let earthquakes = new L.layerGroup();
+
+let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5.0,
+    layers: [street, earthquakes]
+  });
